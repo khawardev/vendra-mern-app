c80a3ca189e652cb646d9f415b77f40748a67c95
Khawar Sultan <khawarsultan.developer@gmail.com>
2023-10-08 18:35:10 +0500

    media compatibility changes

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -14,24 +14,23 @@ const UserPage = () => {
 
 
     return (
-        <div className=" w-11/12 m-auto py-16 px-12 border my-10 rounded-xl border-yellow-500">
-            <section className=' flex justify-between items-center'>
-                <div>
-                    <p className=" text-lg text-gray-500 ">Manage Your account</p>
-                    <p className=" text-lg text-gray-500  my-3 ">Click <span className=" text-black hover:cursor-pointer px-[10px] p-[3px] bg-gray-200 hover:bg-gray-100 rounded-full border mx-[2px] border-yellow-500">here</span> to edit your information</p>
-
+        <div className=" w-11/12 m-auto md:py-16 md:px-12 py-6  md:border my-10 rounded-xl md:border-yellow-500">
+            <section className='md:flex flex-row-reverse justify-between items-center'>
+                <div className='rounded-full  md:mb-0 mb-6 flex justify-center'>
+                    <img src={User} className='rounded-full p-1 border-2 md:w-[75px] w-[130px]' alt="" />
                 </div>
-                <div className='p-1 rounded-full border-2'>
-                <img src={User} width={70} className=' rounded-full' alt="" />
-
+                <div>
+                    <p className="text-lg text-gray-500">Manage Your account</p>
+                    <p className="text-lg text-gray-500 my-3">Click <span className="text-black hover:cursor-pointer px-[10px] p-[3px] bg-gray-200 hover:bg-gray-100 rounded-full border mx-[2px] border-yellow-500">here</span> to edit your information</p>
                 </div>
             </section>
 
 
+
             <p className=" text-2xl font-bold my-10 ">Your account details</p>
 
-            <main className="flex gap-6">
-                <section className=" border border-yellow-500 p-10 w-full rounded-xl shadow">
+            <main className="grid md:grid-cols-2 grid-cols-1 gap-6">
+                <section className=" border border-yellow-500 md:px-10 md:py-10 px-5 py-10 w-full rounded-xl shadow">
                     <p className="text-gray-500  ">PERSONAL</p>
                     <p className="text-gray-500  my-4"> <span className=" text-black font-bold">EMAIL: </span> <span className='ml-3'>khawarsultan@gmail.com</span> </p>
                     <p className="text-gray-500  my-4"> <span className=" text-black font-bold">USERNAME: </span> <span className='ml-3'>Khawar Sultan</span>  </p>
@@ -45,7 +44,7 @@ const UserPage = () => {
                         </button>
                     </div>
                 </section>
-                <section className=" border border-yellow-500 p-10 w-full rounded-xl shadow">
+                <section className=" border border-yellow-500 md:px-10 md:py-10 px-5 py-10 w-full rounded-xl shadow">
                     <p className="text-gray-500 ">USER HISTORY</p>
                     <p className="text-gray-500  my-4"> <span className=" text-black font-bold">USER SINCE: </span> <span className='ml-3'>Oct 2023</span> </p>
                     <p className="text-gray-500  my-4"> <span className=" text-black font-bold">USER PURCHASES: </span><span className='ml-3'>  54</span></p>

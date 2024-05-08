215837b99c1f950b56c400839755c889448c2b6d
nsuubhan62 <ranasubhansoj@gmail.com>
2024-05-08 10:34:36 +0500

    reivew model

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,29 @@
-/* eslint-disable react/no-deprecated */
-import { createRoot } from 'react-dom';
-import App from './App.jsx';
-import { Provider } from 'react-redux';
-import store from './toolkit/store.jsx';
-// import { Toaster } from 'react-hot-toast';
-
-// Use createRoot instead of ReactDOM.render
-createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
-        <App />
-        {/* <Toaster /> */}
-    </Provider>
-);
-// import ReactDOM from 'react-dom';
+// /* eslint-disable react/no-deprecated */
+// import { createRoot } from 'react-dom';
 // import App from './App.jsx';
 // import { Provider } from 'react-redux';
 // import store from './toolkit/store.jsx';
-// import { Toaster } from 'react-hot-toast';
-// import ErrorBoundary from './ErrorBoundary.jsx'
-// ReactDOM.render(
+// // import { Toaster } from 'react-hot-toast';
+
+// // Use createRoot instead of ReactDOM.render
+// createRoot(document.getElementById('root')).render(
 //     <Provider store={store}>
-//           <ErrorBoundary>
 //         <App />
-//         </ErrorBoundary>
-//         <Toaster />
-//     </Provider>,
-//     document.getElementById('root')
+//         {/* <Toaster /> */}
+//     </Provider>
 // );
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import { Provider } from 'react-redux';
+import store from './toolkit/store.jsx';
+import { Toaster } from 'react-hot-toast';
+import ErrorBoundary from './ErrorBoundary.jsx'
+ReactDOM.render(
+    <Provider store={store}>
+          <ErrorBoundary>
+        <App />
+        </ErrorBoundary>
+        <Toaster />
+    </Provider>,
+    document.getElementById('root')
+);

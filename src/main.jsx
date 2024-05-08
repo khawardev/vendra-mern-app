e003cadb93e69d3b1d0c7832b2f29905fe4c6733
Khawar Sultan <khawarsultan.developer@gmail.com>
2024-05-08 10:52:12 +0500

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-// /* eslint-disable react/no-deprecated */
-// import { createRoot } from 'react-dom';
-// import App from './App.jsx';
-// import { Provider } from 'react-redux';
-// import store from './toolkit/store.jsx';
-// // import { Toaster } from 'react-hot-toast';
-
-// // Use createRoot instead of ReactDOM.render
-// createRoot(document.getElementById('root')).render(
-//     <Provider store={store}>
-//         <App />
-//         {/* <Toaster /> */}
-//     </Provider>
-// );
-import ReactDOM from 'react-dom';
-=======
 /* eslint-disable react/no-deprecated */
 // import { createRoot } from 'react-dom/client';
 // import App from './App.jsx';
@@ -30,22 +13,10 @@ import ReactDOM from 'react-dom';
 // );
 
 
->>>>>>> af7d35543d77f829d5cc9fb26b6d61c262d635b0
 import App from './App.jsx';
 import { Provider } from 'react-redux';
 import store from './toolkit/store.jsx';
 import { Toaster } from 'react-hot-toast';
-<<<<<<< HEAD
-import ErrorBoundary from './ErrorBoundary.jsx'
-ReactDOM.render(
-    <Provider store={store}>
-          <ErrorBoundary>
-        <App />
-        </ErrorBoundary>
-        <Toaster />
-    </Provider>,
-    document.getElementById('root')
-=======
 import ErrorBoundary from './ErrorBoundary.jsx';
 import { createRoot } from "react-dom/client";
 
@@ -59,5 +30,4 @@ root.render(
         </ErrorBoundary>
         <Toaster />
     </Provider>
->>>>>>> af7d35543d77f829d5cc9fb26b6d61c262d635b0
 );
